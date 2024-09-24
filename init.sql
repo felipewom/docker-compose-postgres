6e6c8a48da005905cfc936b4a022f443a7bb8fb3
Ludger <ludger.pottmeier@gmail.com>
2024-09-25 00:08:00 +0200

    extend with db2rest

--- init.sql
+++ init.sql
@@ -9,3 +9,14 @@ CREATE TABLE test(
 INSERT INTO test (name, archived)
   VALUES ('test row 1', true),
   ('test row 2', false);
+
+-- db2rest test
+CREATE TABLE employee (
+    id serial4 NOT NULL,
+    first_name varchar(50) NOT NULL,
+    last_name varchar(50) NOT NULL,
+    email varchar(255) NOT NULL,
+    created_on timestamp NOT NULL,
+    CONSTRAINT employee_email_key UNIQUE (email),
+    CONSTRAINT employee_pkey PRIMARY KEY (id)
+);
---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Verzeichnis erstellen, falls es nicht existiert
+if [ ! -d "${PWD}/tmp/db_data" ]; then
+  mkdir -p -m 777 ${PWD}/tmp/db_data 
+fi
+
+# Verzeichnis erstellen, falls es nicht existiert
+if [ ! -d "${PWD}/tmp/pgadmin_data" ]; then
+  mkdir -p  -m 777 ${PWD}/tmp/pgadmin_data
+fi
+
+# Docker Compose starten
+podman compose up
